6e558aaa1d9cf49c4239987a842843dd19a5d519
juandiegosa <juandie_sa@hotmail.com>
2024-01-19 14:36:19 -0700

    creacion de las ventanas: frmCAT_ClasificacionExp, frmCAT_IVA y frmCAT_TiposVehiculo

---
+++ SistProduccionEnC/Catalogos/frmCAT_IVA.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistProduccionEnC.Catalogos
+{
+    /// <summary>
+    /// Interaction logic for frmCAT_IVA.xaml
+    /// </summary>
+    public partial class frmCAT_IVA : Window
+    {
+        public frmCAT_IVA()
+        {
+            InitializeComponent();
+        }
+    }
+}
