9844da9ed78edfd9f532aa2e4a0561ee856ed248
juandiegosa <juandie_sa@hotmail.com>
2024-01-22 15:06:24 -0700

    ventanas juan diego

--- SistProduccionEnC/Catalogos/frmCAT_ClasificacionExp.xaml.cs
+++ SistProduccionEnC/Catalogos/frmCAT_ClasificacionExp.xaml.cs
@@ -23,5 +23,20 @@ namespace SistProduccionEnC.Catalogos
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
