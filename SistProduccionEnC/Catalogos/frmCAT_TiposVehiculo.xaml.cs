f460182a3c0919c83d5127c60fac9c4e85ba0ffa
Jonathan Noriega <jonathannoriega.urias@gmail.com>
2024-01-23 14:45:56 -0700

    modificacion de la ventana tipo de vehiculo

--- SistProduccionEnC/Catalogos/frmCAT_TiposVehiculo.xaml.cs
+++ SistProduccionEnC/Catalogos/frmCAT_TiposVehiculo.xaml.cs
@@ -10,19 +10,33 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SistProduccionEnC.Catalogos
 {
     /// <summary>
-    /// Interaction logic for frmCAT_TiposVehiculo.xaml
+    /// Lógica de interacción para frmCAT_TiposVehiculo.xaml
     /// </summary>
-    public partial class frmCAT_TiposVehiculo : UserControl
+    public partial class frmCAT_TiposVehiculo : Window
     {
         public frmCAT_TiposVehiculo()
         {
             InitializeComponent();
+        }
+
+        private void btn_guardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_guardar_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_NUEVO_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
