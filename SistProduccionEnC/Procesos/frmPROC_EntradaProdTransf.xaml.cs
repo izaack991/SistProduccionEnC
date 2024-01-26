6518ac7f67249dccc527a56e90e11b8ef13ad29f
KEV1NVAll3 <kevin.vall328@gmail.com>
2024-01-25 17:15:33 -0700

    Todos los formularios pendientes de procesos

---
+++ SistProduccionEnC/Procesos/frmPROC_EntradaProdTransf.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistProduccionEnC.Procesos
+{
+    /// <summary>
+    /// Lógica de interacción para frmPROC_EntradaProdTransf.xaml
+    /// </summary>
+    public partial class frmPROC_EntradaProdTransf : Window
+    {
+        public frmPROC_EntradaProdTransf()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow MdiParent { get; internal set; }
+    }
+}
